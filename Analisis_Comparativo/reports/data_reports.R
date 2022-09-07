95f7f3f58d1fd46ac6a48c3f42337819760af868
nselem <nselem35@hotmail.com>
2022-09-07 09:59:18 -0500

    22.09.07

--- Analisis_Comparativo/reports/data_reports.R
+++ Analisis_Comparativo/reports/data_reports.R
@@ -25,20 +25,36 @@ View(merged_metagenomes@tax_table@.Data)
 colnames(merged_metagenomes@tax_table@.Data)<- c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species")
 View(merged_metagenomes@tax_table@.Data)
 
-# para explorara que Phylum tenemos 
+# para explorar a que Phylum tenemos 
 unique(merged_metagenomes@tax_table@.Data[,"Phylum"])
+# cuantos OTUS estan clasificados en filo Firmicutes
+sum(merged_metagenomes@tax_table@.Data[,"Phylum"] == "Firmicutes")
+# cuantos son "Proteobacteria"
+sum(merged_metagenomes@tax_table@.Data[,"Phylum"] == "Proteobacteria")
 
-#open a pdf to save the plot
+
+
+
+merged_metagenomes <- subset_taxa(merged_metagenomes, Kingdom == "Bacteria")
+merged_metagenomes
+
+sample_sums(merged_metagenomes)
+# tenemos una muestra de 1048070 reads
+summary(merged_metagenomes@otu_table@.Data)
+
+
+# queremos visualizar la diversidad (Alpha)
 pdf("richness_plot.pdf") 
-plot_richness(physeq = merged_metagenomes, 
+p <- plot_richness(physeq = merged_metagenomes, 
                 measures = c("Observed","Chao1","Shannon")) 
 # Close the pdf file
 dev.off() 
+ggsave("richness_plot.pdf", plot = p)
 
 
 
 # metadatos... tres tablas... maiz/chile/tomate
-
+# 
 
 
 
